f5d432141f0840eb5fc6b285dd3a7d87831f88ad
Vasilis <bouzasbasilis@hotmail.com>
2021-04-28 17:28:54 +0200

    convert to node and links

--- examples/flowMap.js
+++ examples/flowMap.js
@@ -1,6 +1,16 @@
 import {FlowMap} from '../index.js'
 import flows from '../data/flows'
 
+//var data = []
+//for (var i=0; i < 10000; i++) {
+//    var off = i * 0.1;
+//    flows.forEach(function(f) {
+//        var add = JSON.parse(JSON.stringify(f));
+//        add.source.lon += off;
+//        data.push(add)
+//    })
+//}
+//console.log(data.length)
 
 const map = new FlowMap({
     target: "root",
--- src/FlowMap.js
+++ src/FlowMap.js
@@ -38,9 +38,7 @@ export default class FlowMap extends Map {
         this.colorBy = options.colorBy;
         this.colors = this._getColors();
 
-        this.origins = [];
-        this.destinations = [];
-        this._draw();
+        this._render();
     }
 
     // define category colors
@@ -64,9 +62,60 @@ export default class FlowMap extends Map {
     }
 
     // draw
-    _draw() {
+    _render() {
+        // if not nodes or links in data,
+        // convert to links & nodes
+        if (!this.data.nodes || !this.data.links) this._transformToLinksAndNodes();
+
         //this._drawNodes();
         this._drawFlows();
+    }
+
+    // convert data to links & nodes
+    _transformToLinksAndNodes() {
+        var nodes = new Set(),
+            links = [];
+
+        // split data to nodes & links
+        this.data.forEach(function(flow) {
+            // load unique nodes
+            var source = JSON.stringify(flow.source),
+                target = JSON.stringify(flow.target);
+            nodes.add(source);
+            nodes.add(target);
+
+            // load link
+            flow.source = source;
+            flow.target = target;
+            links.push(flow);
+        })
+
+        // link -> source / target
+        nodes = Array.from(nodes);
+        var idx = {};  // keep track of indexes
+        nodes.forEach(function(n, i) {
+            idx[n] = i;
+        })
+        links.forEach(function(l) {
+            l.source = idx[l.source];
+            l.target = idx[l.target];
+        })
+
+        // source / target -> link
+        nodes.forEach(function(n, i) {
+            nodes[i] = JSON.parse(n);
+            nodes[i].sourceLinks = [];
+            nodes[i].targetLinks = [];
+        })
+        links.forEach(function(l, i) {
+            nodes[l.source].sourceLinks.push(i);
+            nodes[l.target].targetLinks.push(i);
+        })
+
+        this.data = {
+            nodes: nodes,
+            links: links
+        }
     }
 
     _drawNodes() {
@@ -110,26 +159,27 @@ export default class FlowMap extends Map {
             var sx = 0.4,
                 sy = 0.1;
 
+            function bezier(points) {
+                // Set control point inputs
+                var source = {x: points[0][0], y: points[0][1]},
+                    target = {x: points[1][0], y: points[1][1]},
+                    dx = source.x - target.x,
+                    dy = source.y - target.y;
+                    sx -= 0.3 / features.length,
+                    sy += 0.3 / features.length;
+
+                // bezier or arc
+                var controls = [sx * dx, sy * dy, sy * dx, sx * dy];
+
+                return "M" + source.x + "," + source.y +
+                    "C" + (source.x - controls[0]) + "," + (source.y - controls[1]) +
+                    " " + (target.x + controls[2]) + "," + (target.y + controls[3]) +
+                    " " + target.x + "," + target.y;
+            };
+
             features.forEach(function(d) {
-                var o = _this.projection([d.origin.lon, d.origin.lat]),
-                    d = _this.projection([d.destination.lon, d.destination.lat]);
-
-                function bezier(points) {
-                    // Set control point inputs
-                    var source = {x: points[0][0], y: points[0][1]},
-                        target = {x: points[1][0], y: points[1][1]},
-                        dx = source.x - target.x,
-                        dy = source.y - target.y;
-                        sx -= 0.3 / features.length,
-                        sy += 0.3 / features.length;
-                    //bezier or arc
-                    var controls = [sx * dx, sy * dy, sy * dx, sx * dy];
-
-                    return "M" + source.x + "," + source.y +
-                        "C" + (source.x - controls[0]) + "," + (source.y - controls[1]) +
-                        " " + (target.x + controls[2]) + "," + (target.y + controls[3]) +
-                        " " + target.x + "," + target.y;
-                };
+                var o = _this.projection([d.source.lon, d.source.lat]),
+                    d = _this.projection([d.target.lon, d.target.lat]);
 
                 _this.svg.append('path')
                 .attr('d', bezier([o, d]))
@@ -146,7 +196,6 @@ export default class FlowMap extends Map {
         });
         this.map.addLayer(flowsLayer);
     }
-
 }
 
 
